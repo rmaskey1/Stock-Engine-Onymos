c021c713e11774afa396669eb63fc110f94fb327
rmaskey1 <103459061+rmaskey1@users.noreply.github.com>
2025-03-07 15:45:47 -0800

    Update orderbook.py

--- python/orderbook.py
+++ python/orderbook.py
@@ -8,10 +8,11 @@ class OrderBook:
 
     def insert_order(self, order):
         if order.order_type == "BUY":
-            # Insert into buy list in descending order
+            # If first order in list
             if self.buy_head is None or order.price > self.buy_head.price:
                 order.next = self.buy_head
                 self.buy_head = order
+            # Normal insert in descending order
             else:
                 current = self.buy_head
                 while current.next is not None and current.next.price >= order.price:
@@ -19,10 +20,11 @@ class OrderBook:
                 order.next = current.next
                 current.next = order
         elif order.order_type == "SELL":
-            # Insert into sell list in ascending order
+            # If first order in list
             if self.sell_head is None or order.price < self.sell_head.price:
                 order.next = self.sell_head
                 self.sell_head = order
+            # Normal insert in ascending order
             else:
                 current = self.sell_head
                 while current.next is not None and current.next.price <= order.price:
@@ -39,12 +41,15 @@ class OrderBook:
             
             print(f"[Ticker {self.buy_head.ticker}] Order matched: {match_quantity} shares at ${match_price}")
 
+            # Subtracting matched shares from total shares of each BUY and SELL order
             self.buy_head.quantity -= match_quantity
             self.sell_head.quantity -= match_quantity
 
+            # If all BUY order shares are satisfied
             if self.buy_head.quantity <= 0:
                 satisfied_history.append(f"BUY order satisfied: [Ticker {self.buy_head.ticker}] {self.buy_head.order_type} {match_quantity} @ ${self.buy_head.price}")
                 self.buy_head = self.buy_head.next
+            # If all SELL order shares are satisfied
             if self.sell_head is not None and self.sell_head.quantity <= 0:
                 satisfied_history.append(f"SELL order satisfied: [Ticker {self.sell_head.ticker}] {self.sell_head.order_type} {match_quantity} @ ${self.sell_head.price}")
                 self.sell_head = self.sell_head.next
